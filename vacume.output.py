8ae510add03bc0680bd5add29dda17774064e56d
lalithya2120 <lalithyap.eai24@chanakyauniversity.edu.in>
2025-09-04 19:28:24 +0530

    Add files via upload

---
+++ vacume.output.py
@@ -0,0 +1,46 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/ADMIN/OneDrive/Desktop/vacume.py ==============
+
+--- Vacuum Cleaner Menu ---
+1. Start Cleaning
+2. Turn Left
+3. Turn Right
+4. Dock Vacuum
+5. Stop Program
+Enter choice (1-5): 1
+
+Vacuum Started
+1. Clean Solid
+2. Clean Liquid
+3. Back to Main Menu
+Choose option (1-3): 1
+
+Choose Solid Type:
+1. Dust
+2. Rocks / Papers
+3. Others
+Enter choice (1-3): 1
+
+--- Cleaning Results ---
+Category: Solid
+Item: Dust
+
+Best Shape: Round
+Cells Cleaned: 100
+Time Taken: 100 steps
+
+Time Taken by Each Shape:
+- Round: 100 steps
+- Square: 200 steps
+- Triangle: 300 steps
+- Heptagon: 150.0 steps
+
+
+--- Vacuum Cleaner Menu ---
+1. Start Cleaning
+2. Turn Left
+3. Turn Right
+4. Dock Vacuum
+5. Stop Program
