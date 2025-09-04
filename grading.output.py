b93fc5cb8be0c9ae8098bbf945caef31d36a039e
lalithya2120 <lalithyap.eai24@chanakyauniversity.edu.in>
2025-09-04 22:18:34 +0530

    Add files via upload

---
+++ grading.output.py
@@ -0,0 +1,20 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/ADMIN/OneDrive/Desktop/Github homeworks/grading.py =====
+- Grading as per Final Results -
+Enter subject name and marks. Type 'done' when finished.
+
+Enter subject name (or 'done' to finish): maths
+Enter marks in maths: 87
+maths: 87 -> Grade B
+
+Enter subject name (or 'done' to finish): english
+Enter marks in english: 92
+english: 92 -> Grade A
+
+Enter subject name (or 'done' to finish): done
+----- Final Result -----
+Total Marks: 179
+Average: 89.5
+Final Grade: B
