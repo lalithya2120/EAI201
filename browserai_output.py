ed699d1f6685727f087759f060107b81c63facf6
lalithya2120 <lalithyap.eai24@chanakyauniversity.edu.in>
2025-09-04 22:14:51 +0530

    Add files via upload

---
+++ browserai_output.py
@@ -0,0 +1,32 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/ADMIN/OneDrive/Desktop/Github homeworks/browserai.py ====
+Simulated Navigation Task for Chanakya University
+--------------------------------------------------
+
+1. Open the homepage: https://chanakyauniversity.in/
+Press Enter when you finish this step...
+Now, note down the main heading/title you see on this page.
+
+2. Scroll and click on 'Programs' or 'Acquire' (whichever is shown).
+Press Enter when you finish this step...
+Now, note down the main heading/title you see on this page.
+
+3. From the menu, click on 'Undergraduate Programmes'.
+Press Enter when you finish this step...
+Now, note down the main heading/title you see on this page.
+
+4. On that page, click on 'School of Engineering'.
+Press Enter when you finish this step...
+Now, note down the main heading/title you see on this page.
+
+5. Then, go to 'School of Engineering and Social Sciences'.
+Press Enter when you finish this step...
+Now, note down the main heading/title you see on this page.
+
+6. Finally, visit the 'About Chanakya University' page.
+Press Enter when you finish this step...
+Now, note down the main heading/title you see on this page.
+
+All steps are completed. End of simulation.
